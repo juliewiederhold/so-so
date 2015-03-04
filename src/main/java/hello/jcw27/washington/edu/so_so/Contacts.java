ba033d7c2632e50a076facd02ebe6eb54b06241f
juliewiederhold <j_wiederhold@comcast.net>
2015-03-04 15:20:13 -0800

    error and i don't know why :(

--- src/main/java/hello/jcw27/washington/edu/so_so/Contacts.java
+++ src/main/java/hello/jcw27/washington/edu/so_so/Contacts.java
@@ -1,18 +1,61 @@
 package hello.jcw27.washington.edu.so_so;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 
-public class Contacts extends Activity {
+public class Contacts extends ActionBarActivity implements Serializable {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        /*final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                TextView tabSelect = (TextView) findViewById(R.id.tab1);
+                tabSelect.setText("Tab is selected!");
+            }
+
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // hide the given tab
+                TextView tabUnselect = (TextView) findViewById(R.id.tab1);
+                tabUnselect.setText("Tab is unselected!");
+            }
+
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // probably ignore this event
+                TextView tabReselect = (TextView) findViewById(R.id.tab1);
+                tabReselect.setText("Tab is reselected!");
+            }
+        };
+
+        // Add 3 tabs, specifying the tab's text and TabListener
+        for (int i = 0; i < 3; i++) {
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText("Tab " + (i + 1))
+                            .setTabListener(tabListener));
+        }*/
     }
+
 
 
     @Override
